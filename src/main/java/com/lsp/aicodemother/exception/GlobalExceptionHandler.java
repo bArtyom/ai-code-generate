637231571bf9da4adf834a5b18158747bf4bcc5f
bArtyom <2497416595@qq.com>
2025-12-06 16:31:22 +0800

    初始化

---
+++ src/main/java/com/lsp/aicodemother/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.lsp.aicodemother.exception;
+
+
+import com.lsp.aicodemother.common.BaseResponse;
+import com.lsp.aicodemother.common.ResultUtils;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Hidden
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e){
+        log.error("businessException: " , e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e){
+        log.error("runtimeException: " , e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR);
+    }
+}
